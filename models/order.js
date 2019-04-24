be8b9ff7995f48fd9e25a9657dd46c054cf0f286
noviirna <novi.irnawati@gmail.com>
2019-04-24 19:53:51 +0700

    finalize model dan db

--- models/order.js
+++ models/order.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const Order = sequelize.define('Order', {
     order_date: DataTypes.DATE,
     CustomerId: DataTypes.INTEGER,
-    DriverId: DataTypes.INTEGER
+    DriverId: DataTypes.INTEGER,
+    order_status : DataTypes.INTEGER
   }, {});
   Order.associate = function(models) {
     // associations can be defined here

5dae222d9956eb45650515bd6cf188cf32f21050
rudyfebrianthomas <rudyfebrian6@gmail.com>
2019-04-25 14:14:01 +0700

    test index

--- routing/index.js
+++ routing/index.js
@@ -3,6 +3,9 @@ let Customer = require("./customer")
 let Driver = require("./driver")
 let Order = require("./order")
 
+router.get("/", (req, res) => {
+    res.render("home.ejs")
+})
 router.use("/", Customer)
 router.use("/", Driver)
 router.use("/", Order)
